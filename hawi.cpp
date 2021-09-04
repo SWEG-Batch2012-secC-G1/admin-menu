86f0717e40c7b26074924ed1e53e1eb5406fb328
Hawigirmachew <88828065+Hawigirmachew@users.noreply.github.com>
2021-09-04 15:18:21 +0300

    Update hawi.cpp

--- hawi.cpp
+++ hawi.cpp
@@ -1,45 +1,84 @@
 #include<iostream>
 using namespace std;
 
-
+struct product{
+	int product_id;
+	string product_id_add;
+	string product_name;
+	int quantity;
+	float price;
+	float discount;//by percent
+	string product_type;
+	
+}add_pro;
+void add_product(product added_pro);
+  //void show_add_product(product added_pro);
 void Admin_menu(){
+ int option;
+	switch(option)
+	{
+		system("cls");
+	case 1: 
+	add_product(add_pro);
+	break;
+	
+	case 2: 
+	show_add_product(add_pro);
+	break;
+	
+	//case 3:
+}
  
+}
+int main(){
+int option;
+
+	
 	system("cls");
-	int option;
 	cout<<"\t@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@";
 	cout<<"\n\tPress 1 to ADD PRODUCT";
 	cout<<"\n\tPress 2 to DISPLAY ALL PRODUCTS";
-	cout<<"\n\tPress 3 to DELETE PRODUCT";
+//	cout<<"\n\tPress 3 to DELETE;
 	cout<<"\n\tPress 4 to GO BACK TO MAIN MENU";
 	cout<<"\n\t@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@";
 	 
 	cout<<"\n\n\tOption: ";
 	cin>>option;
-	switch(option)
-	{
-	case 1: system("cls");
-	//add_product();
-	break;
-	
-	case 2: //show_all_product();
-	break;
-	
-	case 3:
-	int num;
-	system("cls");	
-	cout<<"\n\n\tPlease Enter The Product Number: ";
-	cin>>num;
-	//display_record(num);
-	break;
-	
-	case 4: //edit_product();
-	break;
+	Admin_menu();
+}
+void 	add_product(product added_pro){
+	product add[100];
+char press;
+int num;
+       cout<<"**********************  The programm already have clothes,shoes and bags.  ****************************\n";
+	   cout<<"*********************  Do you want to add another type of product?"<<endl;
+	   cout<<"Press Y to continue or N to go back to menu\n";
+	   cin>>press;
+	   if(press='Y'){
+	   	cout<<"What is the your Product type: "<<endl;
+	   	cin>>added_pro.product_type;
+	   	cout<<"How many products are their in your product type: "<<endl;
+	   	cin>>num;
+	   	for(int i=0;i<num;i++){
+	   		cout<<"The product in "<<i+1<<endl;
+	   		cout<<"Enter the ID of the Product : ";
+	   		cin>>add[i].product_id_add;
+	   		cout<<"The Name of the Product : ";
+	   		cin>>add[i].product_name;
+	   		cout<<"The price of the product : ";
+	   		cin>>add[i].price;
+	   		cout<<"THe quantity of your product :";
+	   		cin>>add[i].quantity;
+	   		cout<<"The dicount of the Product : ";
+	   		cin>>add[i].discount;
+	   		
+	   		cout<<endl;
+	   		
+	   		
+	   		
+		   }
+	   	
+	   	}
 
-	case 5: //delete_product();
-	break;
-		case 6: system("cls");
-	break;
-	
-	default:Admin_menu();
-	}
-}	 
+	   	
+}
